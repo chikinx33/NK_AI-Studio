28512ef77a1c0c374f7d20c1bbfd7afdfdff1a27
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-01-30 18:44:26 +0900

    Update script.js

--- prototype/script.js
+++ prototype/script.js
@@ -200,7 +200,7 @@
   let theme = 'dark';
   const DRAFT_KEY = 'nk_scenario_drafts_v1';
   const PIPELINE_KEY = 'nk_pipeline_last';
-  const APP_VERSION = '1.093';
+  const APP_VERSION = '1.094';
   let scenesState = [];
   let lastPayload = null;
   let pipelineState = null;
