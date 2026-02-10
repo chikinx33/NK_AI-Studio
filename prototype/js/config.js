c414fca087da9daaabfd11a8eefd87a68e148e21
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-02-10 23:01:31 +0900

    1

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -1,8 +1,8 @@
-; (function () {
+﻿; (function () {
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.506';
+    config.APP_VERSION = '1.507';
 
     // Storage Keys
     config.KEYS = {
