aabe7c14b018d24622e5145f899aaf47c3195f15
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-12 20:12:46 +0900

    Center favorite page dots and simplify box icon

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.722';
+    config.APP_VERSION = '1.723';
 
     // Storage Keys
     config.KEYS = {
