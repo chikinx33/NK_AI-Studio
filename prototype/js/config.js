ebb62f0e2e88d6f9619cf270fd68b3f554c8cb03
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-12 21:44:33 +0900

    Polish iframe theme and scrolling

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.725';
+    config.APP_VERSION = '1.726';
 
     // Storage Keys
     config.KEYS = {
