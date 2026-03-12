272fd9673e49d5214db6e694ce603b451af56e0b
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-12 23:02:11 +0900

    Reorder sidebar navigation

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.730';
+    config.APP_VERSION = '1.731';
 
     // Storage Keys
     config.KEYS = {
