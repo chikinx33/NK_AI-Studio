4b91914bd1cc735d42a4ef55af9c9ec458a6d174
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-14 20:05:38 +0900

    브랜드 자산 필터 테마 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.784';
+    config.APP_VERSION = '1.785';
 
     // Storage Keys
     config.KEYS = {
