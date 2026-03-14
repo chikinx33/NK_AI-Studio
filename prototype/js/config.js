564cb67f1db8f76fd7f89d57aaef113da70e87a4
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-14 18:45:44 +0900

    씬 카드 높이 여백 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.765';
+    config.APP_VERSION = '1.766';
 
     // Storage Keys
     config.KEYS = {
