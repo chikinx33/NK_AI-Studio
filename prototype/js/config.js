afc33c5a41f0fdbd1041234a6c2af2d815c1d96f
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-14 20:39:13 +0900

    영상 자동화 상용화 로드맵 문서 추가

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.789';
+    config.APP_VERSION = '1.790';
 
     // Storage Keys
     config.KEYS = {
