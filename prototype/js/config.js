ae16ed0d79b0244e10f3022e9a043e9ecc7ad06c
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 02:10:40 +0900

    사이드바 프로젝트 카드 다크모드 텍스트 색상 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.939';
+    config.APP_VERSION = '1.940';
 
     // Storage Keys
     config.KEYS = {
