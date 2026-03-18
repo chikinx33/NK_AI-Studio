0d2ea9733d184768896fcffb161c47486925d4be
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-18 21:57:07 +0900

    브랜드 관리 화면 좌우 여백 축소

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.903';
+    config.APP_VERSION = '1.904';
 
     // Storage Keys
     config.KEYS = {
