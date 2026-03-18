ade7610fe49e303888100b7871cf73e7b8cf4d5d
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-18 21:24:32 +0900

    브랜드 운영 페이지 2차 UI 디테일 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.898';
+    config.APP_VERSION = '1.899';
 
     // Storage Keys
     config.KEYS = {
