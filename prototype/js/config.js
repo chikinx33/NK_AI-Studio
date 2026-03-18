7b3e4ffbc83e07f416597977a24400b9da46a086
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 01:53:22 +0900

    텍스트영역 리사이즈 핸들 사선만 유지

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.934';
+    config.APP_VERSION = '1.935';
 
     // Storage Keys
     config.KEYS = {
