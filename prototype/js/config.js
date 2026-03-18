121152d2066be210ef477ad60a23c3f5598f0178
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 00:58:24 +0900

    사이드바 알림 제거 및 테마 아이콘 축소

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.926';
+    config.APP_VERSION = '1.927';
 
     // Storage Keys
     config.KEYS = {
