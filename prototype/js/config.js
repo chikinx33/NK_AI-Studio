1e0deed4e5577e1030f03842f4e95186fa4b0905
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-18 21:47:10 +0900

    브랜드 관리 화면 시선 위계 재정렬

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.901';
+    config.APP_VERSION = '1.902';
 
     // Storage Keys
     config.KEYS = {
