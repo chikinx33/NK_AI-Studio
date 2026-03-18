2d100aec63352bcb7ccfe802292ffe76339c9cfd
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 02:05:32 +0900

    프로젝트 카드 삭제 아이콘 크기 및 정렬 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.938';
+    config.APP_VERSION = '1.939';
 
     // Storage Keys
     config.KEYS = {
