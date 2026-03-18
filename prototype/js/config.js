9c0110d884ed582e254f152f6549b91bcf17eef2
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-18 22:00:37 +0900

    브랜드 허브 문맥 요약 정렬 통일

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.904';
+    config.APP_VERSION = '1.905';
 
     // Storage Keys
     config.KEYS = {
