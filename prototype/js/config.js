caff1fe77d6b6be0278902cfdc0940af06df7534
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 19:11:11 +0900

    대시보드 편집 버튼 스타일 통일

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.947';
+    config.APP_VERSION = '1.948';
 
     // Storage Keys
     config.KEYS = {
