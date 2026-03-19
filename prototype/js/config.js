58a6c053834bce18829f7752b0ef3468cbbe4eec
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 19:16:11 +0900

    대시보드 아이콘과 신규 버튼 시인성 개선

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.949';
+    config.APP_VERSION = '1.950';
 
     // Storage Keys
     config.KEYS = {
