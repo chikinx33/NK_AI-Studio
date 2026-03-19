6bc75c28306e6692db050783b1bc4d39a9b0be23
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 19:54:48 +0900

    대시보드 프로젝트 카드 구분선 제거

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.954';
+    config.APP_VERSION = '1.955';
 
     // Storage Keys
     config.KEYS = {
