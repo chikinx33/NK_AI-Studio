606a0ad8fe879eb80fb4d949843dd16ad2c74212
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-20 02:24:00 +0900

    시나리오 페이지 EN 보완: ‘생성된 시나리오가 없습니다.’/‘왼쪽 패널에서 조건 입력 후 시나리오 생성’/‘장면이 없습니다’ 번역 추가, 버전 1.974

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.973';
+    config.APP_VERSION = '1.974';
 
     // Storage Keys
     config.KEYS = {
