de136c96ae40d87d6398b56b70d7888e00ff8aa6
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-19 19:09:06 +0900

    대시보드 라이트 테마 카테고리 색상 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.946';
+    config.APP_VERSION = '1.947';
 
     // Storage Keys
     config.KEYS = {
