9e66a0b979d723215639d2ef4f9caee0d89cc011
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 02:41:18 +0900

    브랜드 페이지 접힘 카드 모서리 포인트 추가

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.016';
+    config.APP_VERSION = '2.017';
 
     // Storage Keys
     config.KEYS = {
