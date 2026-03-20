2d3b06e4db148779d68d3d4f6bb33000650389ed
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 00:05:47 +0900

    파이프라인 common 프롬프트 텍스트 색상 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.991';
+    config.APP_VERSION = '1.992';
 
     // Storage Keys
     config.KEYS = {
