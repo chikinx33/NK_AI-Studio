bd41911327bc665c7eaba7026ae54a7d7bb4a050
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 01:36:40 +0900

    시나리오 카드 간격과 공통 토픽 바 높이 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.004';
+    config.APP_VERSION = '2.005';
 
     // Storage Keys
     config.KEYS = {
