2862448e30ba913855648c14f54527518154095d
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 01:46:27 +0900

    브랜드 허브 기본 토글과 주제 입력 높이 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.008';
+    config.APP_VERSION = '2.009';
 
     // Storage Keys
     config.KEYS = {
