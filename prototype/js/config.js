9286c9db456ee5638bde370f8cc8cdf1e9f8bdf6
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-20 21:35:34 +0900

    대본 카드 내부 로딩 오버레이 제거

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.982';
+    config.APP_VERSION = '1.983';
 
     // Storage Keys
     config.KEYS = {
