a81d0976aa93553b5092c8cdd0f205b52386234a
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 01:24:09 +0900

    접힌 씬 카드 헤더 정렬 보정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '1.998';
+    config.APP_VERSION = '1.999';
 
     // Storage Keys
     config.KEYS = {
