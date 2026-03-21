d4dac9bb9504f755eac5bc89e7972d6afd403cba
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 03:50:12 +0900

    다크모드 IP 포즈 선택 드롭다운 대비 개선

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.051';
+    config.APP_VERSION = '2.052';
 
     // Storage Keys
     config.KEYS = {
