8a26df06983e6302fc0b77e4db29d34d5f556daf
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-21 13:59:15 +0900

    랜딩 테마 프리셋 영문 번역 누락 수정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.037';
+    config.APP_VERSION = '2.038';
 
     // Storage Keys
     config.KEYS = {
