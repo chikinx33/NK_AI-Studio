be661353cbc0e1db50f5c5a8d652e08e41210509
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 22:13:26 +0900

    브랜드 허브 캐릭터 입력 행 레이아웃 안정화

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.089';
+    config.APP_VERSION = '2.090';
 
     // Storage Keys
     config.KEYS = {
