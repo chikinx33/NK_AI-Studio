233dfc3b5128a8df2511e7151dd982516f17026f
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 21:49:20 +0900

    개요와 시나리오 카드 포커스 스타일 통일

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.086';
+    config.APP_VERSION = '2.087';
 
     // Storage Keys
     config.KEYS = {
