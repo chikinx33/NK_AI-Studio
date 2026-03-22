18d922d3e09e3320a599b174d63f60f5daf8099a
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 22:50:37 +0900

    브랜드 스튜디오 입력 포커스 스타일 개선

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.093';
+    config.APP_VERSION = '2.094';
 
     // Storage Keys
     config.KEYS = {
