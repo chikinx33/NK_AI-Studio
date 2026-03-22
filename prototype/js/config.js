95e5e2ece64edfc0d3232208fe9ace448e9ca525
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 22:03:38 +0900

    브랜드 허브 IP 라이브러리 버튼 위치 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.087';
+    config.APP_VERSION = '2.088';
 
     // Storage Keys
     config.KEYS = {
