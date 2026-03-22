145f2dc68369421fc4341fbf8985fd09f4fff5bf
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 21:43:07 +0900

    이야기 캐릭터 표시 영역 스타일 조정

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.084';
+    config.APP_VERSION = '2.085';
 
     // Storage Keys
     config.KEYS = {
