c82f60daee6dcdb8c560d97e0d1d8729069b3dad
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-22 21:39:50 +0900

    이야기 캐릭터 토큰 표시 단순화

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.083';
+    config.APP_VERSION = '2.084';
 
     // Storage Keys
     config.KEYS = {
