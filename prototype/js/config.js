505b8a0f7b33c46db8b188cc17ceda1d0043250a
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 02:39:43 +0900

    랜딩 전용 스타일 분리 4차 적용

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.164';
+    config.APP_VERSION = '2.165';
 
     // Storage Keys
     config.KEYS = {
