ffba09eb0e75a510504bc26aa7c9d918fcbc6f19
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-23 22:25:17 +0900

    AI 이미지: 워크스페이스 하단 패딩 제거로 카드 하단 밀착; 버전 2.137

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.136';
+    config.APP_VERSION = '2.137';
 
     // Storage Keys
     config.KEYS = {
