e73c0f9eaeb9b087bc14df6892fa27d7c2143cf3
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 01:36:55 +0900

    사이드바 카드 간격 확대 및 버튼 3줄 정렬 적용

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.158';
+    config.APP_VERSION = '2.159';
 
     // Storage Keys
     config.KEYS = {
