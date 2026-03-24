274e5bb41eca212e13eab9febe687ba3fa6ec55a
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 19:40:01 +0900

    대시보드 host 파라미터 링크 안정화 재반영 및 버전 2.190

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.188';
+    config.APP_VERSION = '2.190';
 
     // Storage Keys
     config.KEYS = {
