9623ac1aa5b90cecf74d0dbf4c071ba579e92467
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 18:54:51 +0900

    AI 이미지 헤더: '현재 프로젝트'를 '현재 에피소드'로 변경, 매칭 없을 때 '에피소드 없음' 표기, en 동시 반영, 버전 2.185

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.184';
+    config.APP_VERSION = '2.185';
 
     // Storage Keys
     config.KEYS = {
