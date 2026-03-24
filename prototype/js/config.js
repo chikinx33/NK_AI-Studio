b4836ac118eab095923a035efdbe43fc1303ade3
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 17:27:13 +0900

    생성 히스토리 카드 3개 한 행 고정 레이아웃 적용

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.169';
+    config.APP_VERSION = '2.170';
 
     // Storage Keys
     config.KEYS = {
