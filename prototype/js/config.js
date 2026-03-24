2534801e04861388678f3f382a7c902e898edd0d
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-25 08:35:26 +0900

    소스 프리뷰 UI 미세 보정: img-modal-trigger 버튼의 기본 여백/테두리/배경 제거로 외곽 잔여 박스 완전 제거. 앱 버전 2.221

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.220';
+    config.APP_VERSION = '2.221';
 
     // Storage Keys
     config.KEYS = {
@@ -52,7 +52,6 @@
     };
 
 })();
-
 
 
 
