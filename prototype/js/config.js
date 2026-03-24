1f55babe9f621386603e9c195bcd6aa4ea31b6cb
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 18:25:32 +0900

    버튼 아이콘을 SVG로 새로 디자인: 새로고침/복사/다운로드, currentColor 적용, 크기 18px, 버전 2.180

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.179';
+    config.APP_VERSION = '2.180';
 
     // Storage Keys
     config.KEYS = {
