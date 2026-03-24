5993f6de935eb3024aba9e661f511566d85e603d
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 19:56:32 +0900

    Brand Studio 헤드에 셸 전용 대시보드 버튼 숨김 인라인 규칙 추가 및 버전 2.192

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.191';
+    config.APP_VERSION = '2.192';
 
     // Storage Keys
     config.KEYS = {
