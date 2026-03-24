0472533f09a687b8362307e3a3ac037d5d3e5d50
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-25 00:29:02 +0900

    이미지 대시보드에서 시리즈 관리 바 비표시(host==='image' 조건으로 manage bar 제거), 버전 2.206

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.205';
+    config.APP_VERSION = '2.206';
 
     // Storage Keys
     config.KEYS = {
