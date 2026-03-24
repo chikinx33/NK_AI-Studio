97820d36f6135097e951e1b3f150fcc083ca13e9
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 17:40:42 +0900

    AI 이미지 스테이지에 전용 CSS 연결 및 버전 갱신

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.171';
+    config.APP_VERSION = '2.172';
 
     // Storage Keys
     config.KEYS = {
