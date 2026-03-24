d204f1ebf2961878a8e706fcfb49ed44a5ccb980
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-24 18:13:17 +0900

    미리보기 하단 정렬 동기화: 메타·브랜드·버튼 하단 고정, 이미지 영역 유연 확장(flex grow), 버튼 상단 여백 재정렬, 버전 2.176

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.175';
+    config.APP_VERSION = '2.176';
 
     // Storage Keys
     config.KEYS = {
