58d24a75eed05dc101703b3de65cf40d2cd3a069
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-25 21:40:20 +0900

    랜딩 영문 모드: 로그인 상태 버튼 문구를 'Sign out'으로 고정(번역 매핑 수정), 버전 2.230

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.227';
+    config.APP_VERSION = '2.230';
 
     // Storage Keys
     config.KEYS = {
@@ -52,5 +52,3 @@
     };
 
 })();
-
-
