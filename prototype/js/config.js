bd52ff0fcedd5b75ca4897edbb2a240d41f80b89
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-25 23:48:53 +0900

    파비콘 리디자인: N의 오른쪽 수직획과 K의 왼쪽 수직획을 공유하는 단일 축 로고로 단순화. 앱 버전 2.242

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.241';
+    config.APP_VERSION = '2.242';
 
     // Storage Keys
     config.KEYS = {
