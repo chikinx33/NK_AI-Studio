caa943421a02db66795e896c9a0390cda8d11bc9
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-28 03:53:45 +0900

    미리보기 이미지 가로 폭 항상 채우기: 미리보기 이미지 width/height 100% + object-fit: cover로 카드 폭 완전 충전(중앙 크롭)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.277';
+    config.APP_VERSION = '2.278';
 
     // Storage Keys
     config.KEYS = {
