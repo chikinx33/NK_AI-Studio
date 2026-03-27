66cde8ee5b8db1bbd2bcc4ebd1ee588ee03e19dd
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-28 05:05:23 +0900

    앱 버전 2.296 정리

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.295';
+    config.APP_VERSION = '2.296';
 
     // Storage Keys
     config.KEYS = {
