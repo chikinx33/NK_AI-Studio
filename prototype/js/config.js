81bd7990d221c0e23b63b00bebbb140f9992c6de
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-03-28 02:35:43 +0900

    미리보기 영역 70/30 분할: 이미지 70%·하단 메타/버튼 30% 고정 그리드, 겹침 완전 방지 · 앱 버전 2.268

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.267';
+    config.APP_VERSION = '2.269';
 
     // Storage Keys
     config.KEYS = {
