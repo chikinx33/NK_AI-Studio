1b7b5f31dc92d7f2ddc3b3f30ed3eba78b550ecc
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-02 02:32:07 +0900

    버전 갱신 및 테스트 통과 · 앱 버전 2.354

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.352';
+    config.APP_VERSION = '2.354';
 
     // Storage Keys
     config.KEYS = {
