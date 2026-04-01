7bad8ec090d51a02e28fb9b21ac7155973c87528
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-02 00:21:28 +0900

    AI 이미지 소스 박스 하단 여백 제거 · 앱 버전 2.336

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.335';
+    config.APP_VERSION = '2.336';
 
     // Storage Keys
     config.KEYS = {
