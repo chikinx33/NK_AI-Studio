de7f1eaaa1b036315e973095e3695d4b1cc02822
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-01 23:31:22 +0900

    AI 이미지 삭제 버튼 원형 고정 · 앱 버전 2.329

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.328';
+    config.APP_VERSION = '2.329';
 
     // Storage Keys
     config.KEYS = {
