0705559b70fc064cfe95ee2592c4c8aeb3e18c87
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-01 20:48:42 +0900

    공용 스크롤바 스타일 통일 · 앱 버전 2.314

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.313';
+    config.APP_VERSION = '2.314';
 
     // Storage Keys
     config.KEYS = {
