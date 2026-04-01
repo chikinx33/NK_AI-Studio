114eb3d222a3421396d6dbc2947986d6cec3692c
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-01 18:14:11 +0900

    AI 이미지 소스 카드 높이 고정 · 앱 버전 2.310

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.309';
+    config.APP_VERSION = '2.310';
 
     // Storage Keys
     config.KEYS = {
