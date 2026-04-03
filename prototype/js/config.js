fb6be6d9d36cbdab115c2c69c21770c7229357ba
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-04 01:43:45 +0900

    CSS 린트 오류 수정: 잘못된 중괄호/고아 선언 제거 · 빈 규칙 삭제 · line-clamp 표준 속성 추가 · 앱 버전 2.418

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.417';
+    config.APP_VERSION = '2.418';
 
     // Storage Keys
     config.KEYS = {
