38270a83458c5ec66b983f96840bd481b70237d7
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-04-23 14:32:16 +0900

    v2.644 - AI 영상 우측 패널 폭 확장(460px), 이미지 등록 영역 확대(140px)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.643';
+    config.APP_VERSION = '2.644';
 
     // Storage Keys
     config.KEYS = {
