e5a460b676c894e71a0dbe49992152fc1f8a571a
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-04 01:14:06 +0900

    fix: 편집 버전 X 버튼 세로 위치 수정 - absolute → flex 레이아웃으로 변경

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.814';
+    config.APP_VERSION = '2.815';
 
     // Storage Keys
     config.KEYS = {
