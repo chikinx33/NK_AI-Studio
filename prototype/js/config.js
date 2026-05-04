8ffa7827c5f4cb75575f024f6faae14871c8b8b3
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-04 22:26:06 +0900

    chore: apply new env variables (ELEVENLABS_API_KEY, AUDIO_OUTPUT_GCS_URI)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.850';
+    config.APP_VERSION = '2.851';
 
     // Storage Keys
     config.KEYS = {
