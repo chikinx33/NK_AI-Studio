32fa5e5d0506eb6cf38655678d3cbfc7ee6055a8
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-05 20:16:25 +0900

    v2.910: bsf-flow-card 배경 studio-hero 스타일로 통일

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.909';
+    config.APP_VERSION = '2.910';
 
     // Storage Keys
     config.KEYS = {
