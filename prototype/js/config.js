b6be374c0cd52433a406b9e713186602eac7384e
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-05 19:12:07 +0900

    fix: brand-content-type-card border-left 3px 두께 연출 완전 제거 (v2.904)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.903';
+    config.APP_VERSION = '2.904';
 
     // Storage Keys
     config.KEYS = {
