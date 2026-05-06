846f7ca42980a3dc89618089eaf39bf474effc43
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-07 08:31:39 +0900

    브랜드 스튜디오: 범례 텍스트 크기 축소·파란색 v2.956

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.955';
+    config.APP_VERSION = '2.956';
 
     // Storage Keys
     config.KEYS = {
