abf0ca3c52ff4fc9273f05db89c7fcfd262f9492
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-06 12:45:51 +0900

    v2.937: Selected thumbnails show outer orange border instead of inset shadow

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.936';
+    config.APP_VERSION = '2.937';
 
     // Storage Keys
     config.KEYS = {
