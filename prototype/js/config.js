00856addcfa6bf9e1d78ad18603c672c70080d87
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-06 12:35:10 +0900

    v2.934: Remove orange border from unselected compatible format cards

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '2.933';
+    config.APP_VERSION = '2.934';
 
     // Storage Keys
     config.KEYS = {
