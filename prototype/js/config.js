f1f123b3709553720eeb72b17d10ed2170e2b344
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-11 04:52:26 +0900

    fix: left-align Instagram preview card like YouTube Shorts and TikTok
    - Remove margin: 0 auto from .bsf-mock-ig
    - Now matches TikTok / YouTube Shorts left-aligned layout
    - v3.010

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.009';
+    config.APP_VERSION = '3.010';
 
     // Storage Keys
     config.KEYS = {
