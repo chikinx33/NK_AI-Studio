b6424a6271ce4d458cf8ab3056544bf270a20e33
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-11 06:19:59 +0900

    fix: FB/LI action bar left padding and icon centering v3.019

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.018';
+    config.APP_VERSION = '3.019';
 
     // Storage Keys
     config.KEYS = {
