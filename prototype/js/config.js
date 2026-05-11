5224dd93312098fb563793b9fc4a0ff3c628449e
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-12 04:08:31 +0900

    fix: 배포완료 뱃지 체크+텍스트 수직 가운데 정렬, SVG display:block으로 좌우 간격 균등화

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.727';
+    config.APP_VERSION = '3.728';
 
     // Storage Keys
     config.KEYS = {
