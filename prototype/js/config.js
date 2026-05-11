ce87c375b300c58086960f04e9fd436f3b4ac73d
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-11 20:50:00 +0900

    fix(sns): convert sns-settings to no-sidebar layout to avoid duplicate sidebar

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.27';
+    config.APP_VERSION = '3.28';
 
     // Storage Keys
     config.KEYS = {
