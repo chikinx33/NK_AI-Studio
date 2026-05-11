da101f317ca17adc50d2be055b7d306928769493
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-12 03:06:01 +0900

    chore: trigger Cloudflare Pages deploy for v3.713

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.713';
+    config.APP_VERSION = '3.714';
 
     // Storage Keys
     config.KEYS = {
