28bd172b1342e62c1e4b2bc3003c02d3e3888683
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-12 02:55:34 +0900

    v3.710: 버전 번호 소수점 3자리 형식 수정 (3.71 → 3.710)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.71';
+    config.APP_VERSION = '3.710';
 
     // Storage Keys
     config.KEYS = {
