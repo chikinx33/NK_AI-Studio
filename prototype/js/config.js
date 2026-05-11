36bf84aaba37fada19b21656f3e4c94409a019ff
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-12 04:29:29 +0900

    chore: retrigger Cloudflare Pages build (previous run failed to submit job)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.730';
+    config.APP_VERSION = '3.731';
 
     // Storage Keys
     config.KEYS = {
