dcb85245597284c1ef7107d6b7e4d6c0f5da08c0
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-12 18:23:31 +0900

    ai-doc: increase cat tab icons to 28px, apply btn-primary gradient highlight

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.754';
+    config.APP_VERSION = '3.755';
 
     // Storage Keys
     config.KEYS = {
