5f94256546bd9aad76caaadfd4f79d355b97078b
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-14 06:52:12 +0900

    ci: trigger Cloudflare Pages redeploy (env var update)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.798';
+    config.APP_VERSION = '3.799';
 
     // Storage Keys
     config.KEYS = {
