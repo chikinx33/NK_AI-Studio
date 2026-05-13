95ff196a683111e6850e84d850859e27028b4223
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-14 06:08:00 +0900

    ci: trigger Cloudflare Pages deploy

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.795';
+    config.APP_VERSION = '3.796';
 
     // Storage Keys
     config.KEYS = {
