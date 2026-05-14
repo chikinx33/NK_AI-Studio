52147d3b9605f14575ef1d8becda998255d1db66
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-15 02:05:42 +0900

    v3.814: add TikTok Content Posting API URL verification file

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.813';
+    config.APP_VERSION = '3.814';
 
     // Storage Keys
     config.KEYS = {
