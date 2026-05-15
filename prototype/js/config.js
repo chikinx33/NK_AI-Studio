487cbf3c7391602bbeadd6add54de1e2ec85fce9
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-16 06:13:43 +0900

    feat: Facebook OAuth 스코프에 pages_manage_engagement 재추가 (Meta Console 등록 완료) v3.856

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.855';
+    config.APP_VERSION = '3.856';
 
     // Storage Keys
     config.KEYS = {
