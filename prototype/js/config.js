0e3e4cca47e5ddce8058c841fa2b9722daf54f36
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-24 05:36:22 +0900

    재연결 필요 상태: 주황 채움 제거 → 하늘색 아웃라인(라인) 스타일로 연결 해제 표현 (v3.961)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.960';
+    config.APP_VERSION = '3.961';
 
     // Storage Keys
     config.KEYS = {
