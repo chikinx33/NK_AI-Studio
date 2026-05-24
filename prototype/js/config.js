677b5cf242346e9b1effce57c04967b002d124ce
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-24 23:13:08 +0900

    feat: Threads·X 글 생성에 뚜뮤 화자 화법 분기 적용 (v3.977)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.976';
+    config.APP_VERSION = '3.977';
 
     // Storage Keys
     config.KEYS = {
