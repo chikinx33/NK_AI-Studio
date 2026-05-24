bcfc580129a5da74a3f48705dd4761d56d04d024
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-05-24 14:38:46 +0900

    feat: add Google Search Console verification meta tag

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.962';
+    config.APP_VERSION = '3.963';
 
     // Storage Keys
     config.KEYS = {
