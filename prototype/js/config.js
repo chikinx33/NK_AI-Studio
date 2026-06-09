c9261043113058cda4d6ba440c54276f642c863f
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-10 01:34:39 +0900

    style: 사이드바 런처 테두리/배경 제거+아이콘 확대, 풀스크린/테마/한영 토글 삭제, 홈 버튼 축소 후 사이드바 상단 우측 모서리로 이동 (5개 셸)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1025';
+    config.APP_VERSION = '3.1026';
 
     // Storage Keys
     config.KEYS = {
