5395609d60ca53390b16fabd7262cbb2e722d234
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-10 01:13:47 +0900

    fix: 콘텐츠 저장소 빈 화면 수정 - library.html에 js/service/brand.js 누락 추가 (NK.service.brand 미존재로 init 가드에서 불러올 수 없음 처리)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1023';
+    config.APP_VERSION = '3.1024';
 
     // Storage Keys
     config.KEYS = {
