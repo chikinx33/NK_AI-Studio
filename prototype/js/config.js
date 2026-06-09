962811f4a80ad02f9ca908fdc5c2b11710cb31e3
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-10 01:38:18 +0900

    fix: 사이드바 홈 버튼 원래 집 아이콘 복원(인라인 SVG)+정사각형(28x28). 클래스 이동으로 빠졌던 icon-btn::before 집 아이콘을 버튼 내부 SVG로 대체

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1026';
+    config.APP_VERSION = '3.1027';
 
     // Storage Keys
     config.KEYS = {
