4cf5aaa349f5afc248a71895324940358782d9ac
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-10 22:02:07 +0900

    라이브러리 모달 스크롤 복원 (스크롤바 숨김)
    원인: .card의 overflow:hidden(스타일시트 뒤쪽 정의, 동일 명시도)이
    .lib-content의 overflow:auto를 덮어써 라이브러리가 스크롤되지 않음.
    수정: .card.lib-content로 명시도를 높여 overflow:auto 복원, 스크롤바는 비표시.

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1064';
+    config.APP_VERSION = '3.1065';
 
     // Storage Keys
     config.KEYS = {
