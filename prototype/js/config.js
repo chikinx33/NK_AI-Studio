8ac88b6ba2ad965b645c6ffdf28a28219550b161
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-10 20:37:53 +0900

    브랜드 스튜디오 콘텐츠 페이지 세로 스크롤 활성화 (스크롤바 숨김)
    자산 단계 이미지가 많을 때 잘려 보이던 문제 해결: .brand-studio-page를 overflow-y:auto로 변경하고 스크롤바는 비표시 처리

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1058';
+    config.APP_VERSION = '3.1059';
 
     // Storage Keys
     config.KEYS = {
