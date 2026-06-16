385d21dbdcc5356aa9a72c1a47792cd359e907c8
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-06-16 21:39:11 +0900

    ui: 우측 사이드바 프로젝트 아이콘 KanbanIcon→FolderDot 통일 (v3.1170)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1169';
+    config.APP_VERSION = '3.1170';
 
     // Storage Keys
     config.KEYS = {
