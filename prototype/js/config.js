6a3542dc9040e43f933172ca3283075fd5dea954
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-07-07 08:00:10 +0900

    chore: web_fetch 2단계 Browser Rendering 키 반영 재배포 트리거 (v3.1286)

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1285';
+    config.APP_VERSION = '3.1286';
 
     // Storage Keys
     config.KEYS = {
