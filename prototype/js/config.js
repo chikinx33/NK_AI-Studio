6064578662b0865f0196e045571dcf1b5fe0a960
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-07-28 00:46:00 +0900

    분석 화면 정적 자산 캐시 갱신 보장 · 앱 버전 3.1394

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1393';
+    config.APP_VERSION = '3.1394';
 
     // Storage Keys
     config.KEYS = {
