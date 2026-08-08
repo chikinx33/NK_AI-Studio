ac46e9c34412baf77dc6a22979daa751f2f69d5e
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-08-08 17:24:21 +0900

    v3.1497 NK AI Studio 사업계획서 사례 중심 전면 개편

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1496';
+    config.APP_VERSION = '3.1497';
 
     // Storage Keys
     config.KEYS = {
