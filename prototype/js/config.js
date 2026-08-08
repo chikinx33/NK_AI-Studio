a0f48d361e2cbef637dd2c3122770f1ad7d89310
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-08-08 16:27:47 +0900

    NK AI Studio 사업계획서 제작 · 앱 버전 3.1492

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1491';
+    config.APP_VERSION = '3.1492';
 
     // Storage Keys
     config.KEYS = {
