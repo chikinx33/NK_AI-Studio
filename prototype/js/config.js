a0ce5ede3c16ae8d3966be21b8f084c9754efabd
chikinx33 <140688000+chikinx@users.noreply.github.com>
2026-08-08 19:37:45 +0900

    style(favorites): 페이지 이동 버튼 아이콘을 폴더 아이콘으로 통일
    - 좌/우 이동 버튼 모두 lucide folder 아이콘 사용(방향은 좌우 배치로 구분)
    - APP_VERSION 3.1508 및 캐시버스트 갱신

--- prototype/js/config.js
+++ prototype/js/config.js
@@ -2,7 +2,7 @@
     var NK = window.NK || (window.NK = {});
     var config = NK.config || (NK.config = {});
 
-    config.APP_VERSION = '3.1507';
+    config.APP_VERSION = '3.1508';
 
     // Storage Keys
     config.KEYS = {
